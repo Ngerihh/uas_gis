3c24613e1dc41c495fb062177ab0f29b44f1d892
Ngerihh <Ngerihh@gmail.com>
2023-06-05 21:19:07 +0800

    mengupload project uas

---
+++ data/POMKEFA_1.js
@@ -0,0 +1,11 @@
+var json_POMKEFA_1 = {
+"type": "FeatureCollection",
+"name": "POMKEFA_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ID": null, "NAMA POM": null, "JMLAH_KARY": null, "STOK_MNYAK": null, "IMG": null }, "geometry": { "type": "Point", "coordinates": [ 124.479919205199437, -9.46185009597639 ] } },
+{ "type": "Feature", "properties": { "ID": "1", "NAMA POM": "PERTAMINA  BARU", "JMLAH_KARY": "23 Orang", "STOK_MNYAK": "20.000 Kl", "IMG": "D:\\gis\\img\\1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 124.479918621980417, -9.461852397114271 ] } },
+{ "type": "Feature", "properties": { "ID": "2", "NAMA POM": "SPBU NAESLEU", "JMLAH_KARY": "14 Orang", "STOK_MNYAK": "15.000 Kl", "IMG": "D:\\gis\\img\\3.jpeg" }, "geometry": { "type": "Point", "coordinates": [ 124.48289537177925, -9.45783458691254 ] } },
+{ "type": "Feature", "properties": { "ID": "3", "NAMA POM": "PERTAMINA KM4", "JMLAH_KARY": "10 Orang", "STOK_MNYAK": "10.000 Kl", "IMG": "D:\\gis\\img\\2.jpeg" }, "geometry": { "type": "Point", "coordinates": [ 124.481894603283664, -9.471867213034642 ] } }
+]
+}
